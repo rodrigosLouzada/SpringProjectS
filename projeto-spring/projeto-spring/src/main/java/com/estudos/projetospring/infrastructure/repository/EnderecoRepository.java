b7020e30a499d291bf1d96724b0aec3bf0e4fbe0
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2025-05-02 17:04:25 -0300

    creating repositories for usuario,telefone,endereco

---
+++ projeto-spring/projeto-spring/src/main/java/com/estudos/projetospring/infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.estudos.projetospring.infrastructure.repository;
+
+import com.estudos.projetospring.infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
---
+++ projeto-spring/projeto-spring/src/main/java/com/estudos/projetospring/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,9 @@
+package com.estudos.projetospring.infrastructure.repository;
+
+import com.estudos.projetospring.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+}
