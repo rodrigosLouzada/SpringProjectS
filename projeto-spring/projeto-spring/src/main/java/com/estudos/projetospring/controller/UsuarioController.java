a50fc86bd53720599dc3023c7d7e2a3cecfc6f17
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2025-05-05 16:33:57 -0300

    creating usarioController working

--- projeto-spring/projeto-spring/src/main/java/com/estudos/projetospring/controller/UsuarioController.java
+++ projeto-spring/projeto-spring/src/main/java/com/estudos/projetospring/controller/UsuarioController.java
@@ -5,6 +5,7 @@ import com.estudos.projetospring.infrastructure.entity.Usuario;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +17,7 @@ public class UsuarioController {
     private final UsuarioService usuarioService;
 
     @PostMapping
-    public ResponseEntity<Usuario> salvarUsuario(Usuario usuario){
+    public ResponseEntity<Usuario> salvarUsuario(@RequestBody Usuario usuario){
         return ResponseEntity.ok(usuarioService.salvarUsuario(usuario));
     }
 }
