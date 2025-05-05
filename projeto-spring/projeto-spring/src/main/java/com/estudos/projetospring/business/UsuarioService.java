45185d2203f12171915c102a3aecb1519407a4c2
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2025-05-05 13:08:21 -0300

    creating a personal error, cleand code

---
+++ projeto-spring/projeto-spring/src/main/java/com/estudos/projetospring/business/UsuarioService.java
@@ -0,0 +1,39 @@
+package com.estudos.projetospring.business;
+
+import com.estudos.projetospring.infrastructure.entity.Usuario;
+import com.estudos.projetospring.infrastructure.exceptions.ConflictException;
+import com.estudos.projetospring.infrastructure.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    private Usuario salvarUsuario(Usuario usuario){
+        try {
+            emailExiste(usuario.getEmail());
+            return usuarioRepository.save(usuario);
+        }catch (ConflictException e){
+            throw new ConflictException("Email já cadastrado", e.getCause());
+        }
+    }
+
+
+    public void emailExiste(String email){
+        try {
+            boolean existe = verificaEmailExistente(email);
+            if(existe){
+                throw  new ConflictException("Email já cadastrado: " + email);
+            }
+        }catch (ConflictException e){
+            throw new ConflictException("Email já cadastrado" , e.getCause());
+        }
+    }
+
+    public boolean verificaEmailExistente(String email){
+        return usuarioRepository.existsByEmail(email);
+    }
+}
---
+++ projeto-spring/projeto-spring/src/main/java/com/estudos/projetospring/infrastructure/exceptions/ConflictException.java
@@ -0,0 +1,12 @@
+package com.estudos.projetospring.infrastructure.exceptions;
+
+public class ConflictException extends RuntimeException {
+
+    public ConflictException(String mensagem){
+        super(mensagem);
+    }
+
+    public ConflictException(String mensagem, Throwable throwable){
+        super(mensagem);
+    }
+}
